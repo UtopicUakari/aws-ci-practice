82ac1b4e898aff2edc7a9628845db4c83c5b7ce6
shibashis <shibashist@gmail.com>
2020-09-13 09:03:51 +0100

    test against bucket

--- cypress/integration/test-home.spec.ts
+++ cypress/integration/test-home.spec.ts
@@ -1,6 +1,6 @@
 describe("My First Test", () => {
   it("Homepage shows correct heading", () => {
-    cy.visit("http://localhost:3000")
+    cy.visit("http://mytestapp-1234.s3-website-us-east-1.amazonaws.com/")
       .get(".content")
       .should("have.text", "Hello World!");
   });
