2c646430fd656e54de8a77add3f268842c77e57b
shibashis <shibashist@gmail.com>
2020-09-13 16:11:59 +0100

    upgrade to v0.3

--- cypress/integration/test-home.spec.ts
+++ cypress/integration/test-home.spec.ts
@@ -7,6 +7,6 @@ describe("My First Test", () => {
   it("Homepage shows correct version", () => {
     cy.visit("http://mytestapp-1234.s3-website-us-east-1.amazonaws.com/")
       .get(".version")
-      .should("have.text", "V 0.2");
+      .should("have.text", "V 0.3");
   });
 });
