32d02ab76fd59fb8c5bd640489d3ab1a61d780f7
shibashis <shibashist@gmail.com>
2020-09-13 09:16:50 +0100

    added a test

--- cypress/integration/test-home.spec.ts
+++ cypress/integration/test-home.spec.ts
@@ -4,4 +4,9 @@ describe("My First Test", () => {
       .get(".content")
       .should("have.text", "Hello World!");
   });
+  it("Homepage shows correct version", () => {
+    cy.visit("http://mytestapp-1234.s3-website-us-east-1.amazonaws.com/")
+      .get(".version")
+      .should("have.text", "V 0.2");
+  });
 });
