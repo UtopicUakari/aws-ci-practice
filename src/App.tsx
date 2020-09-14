5a0fc7381937a7dc51ee12cece6121d37f192f21
shibashis <shibashist@gmail.com>
2020-09-14 10:26:13 +0100

    Update to v.5

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p className="content">Hello World!</p>
-        <p className="version">V 0.4</p>
+        <p className="version">V 0.5</p>
       </header>
     </div>
   );
